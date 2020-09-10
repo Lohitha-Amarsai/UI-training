9974140614d87026151aad283477d8c8897fd2c4
Lohitha-Amarsai <36123693+Lohitha-Amarsai@users.noreply.github.com>
2020-09-10 10:08:59 +0530

    Add files via upload

---
+++ nodejstraining/app.js
@@ -0,0 +1,10 @@
+var add=require('./add');
+
+console.log(add.addition(4,5));
+
+var b=require('./subtract');
+console.log(b.subtraction(7,6));
+
+var c=require('./mul');
+console.log(c.multiplication(8,9));
+
